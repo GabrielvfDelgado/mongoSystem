7e9336466634a5e3a79e88156466546fc37d5497
Gabriel Delgado <gabrielvfdelgado@gmail.com>
2022-06-30 17:45:23 -0300

    added server.js and upgrade of Package and README

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # mongoSystem
 
-MongoSystem é um projeto de estudo com a ideia de desenvolver, aplicar conhecimentos sobre express e MongoDB.
+MongoSystem é um projeto de estudo com a ideia de desenvolver e aplicar conhecimentos sobre express e MongoDB.
---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require("http");
+const port = 3000;
+
+const rotas = {
+  '/': 'Pagina inicial',
+  '/livros': 'Pagina de livros',
+  '/autores': 'Pagina de autores',
+  '/editora': 'Pagina editora'
+};
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end(rotas[req.url]);
+});
+
+server.listen(port, () => {
+  console.log(`O servidor esta escutando em http://localhost:${port}`);
+});
