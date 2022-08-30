4c80e153da7e1d34906a8622f5ed7d4e2716379d
Gabriel Delgado <gabrielvfdelgado@gmail.com>
2022-08-30 11:43:06 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 const livros = [
   { id: 1, "titulo": "Harry Potter e a pedra filosofal" },
   { id: 2, "titulo": "Harry Potter e a camara secreta" }
 ];
 
+//GETS
 
 app.get('/', (req, res) => {
   res.status(200).send('Livraria');
@@ -15,6 +18,44 @@ app.get('/', (req, res) => {
 app.get('/livros', (req, res) => {
   res.status(200).json(livros);
 });
+
+app.get('/livros/:id', (req, res) => {
+  const index = buscaLivro(req.params.id);
+  res.status(200).json(livros[index]);
+});
+
+
+//POSTS
+
+app.post('/livros', (req, res) => {
+  livros.push(req.body);
+  res.status(201).send('Livro cadastrado');
+});
+
+//PUTS
+
+app.put('/livros/:id', (req, res) => {
+  const { id } = req.params;
+  const index = buscaLivro(id);
+  livros[index].titulo = req.body.titulo;
+  res.json(livros);
+});
+
+//Delete
+
+app.delete('/livros/:id', (req, res) => {
+  const { id } = req.params;
+  const index = buscaLivro(id);
+  livros.splice(index, 1);
+  res.send(`Livro ${id} excluido`);
+});
+
+
+//functions
+
+function buscaLivro(id) {
+  return livros.findIndex(livro => livro.id == id);
+}
 
 
 export default app;
