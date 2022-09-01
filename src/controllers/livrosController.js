8505978bc9864e26e8a11821deb53675aa84b125
Gabriel Delgado <gabrielvfdelgado@gmail.com>
2022-09-01 16:39:58 -0300

    Added controllers and routes 3/4

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -8,6 +8,18 @@ class LivrosController {
     });
   };
 
+  static listarLivrosId = (req, res) => {
+    const id = req.params.id;
+
+    livros.findById(id, (err, livros) => {
+      if (!err) {
+        res.status(200).send(livros);
+      } else {
+        res.status(404).send({ message: err.message });
+      }
+    });
+  };
+
   static cadastrarLivro = (req, res) => {
     let livro = new livros(req.body);
 
@@ -19,6 +31,19 @@ class LivrosController {
       }
     });
   };
+
+  static atualizarLivro = (req, res) => {
+    const id = req.params.id;
+
+    livros.findByIdAndUpdate(id, { $set: req.body }, (err) => {
+      if (!err) {
+        res.status(200).send({ message: 'Livro atualizado com sucesso' });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+  };
+
 }
 
 export default LivrosController;
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 router
   .get("/livros", LivrosController.listarLivros)
+  .get("/livros/:id", LivrosController.listarLivrosId)
+  .put("/livros/:id", LivrosController.atualizarLivro)
   .post("/livros", LivrosController.cadastrarLivro);
 
 export default router;
